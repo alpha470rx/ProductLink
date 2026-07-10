bb5d85a7b715b3aee5bcd636c8b6741fd16cc5a6
𝐚𝐥𝐩𝐡𝐚 <alpha470rx@gmail.com>
2026-07-10 18:51:25 +0700

    Add files via upload

---
+++ ads.js
@@ -0,0 +1,543 @@
+const products = [
+  {
+    title: "ของแท้100%OLLIEครีมกำจัดขน70มล.ใช้ได้ทุกจุดปลอดภัยไม่เจ็บ ผิวเรียบเนียนทันที",
+    price: "฿88.00 ฿211.00 ",
+    image: "a/1.jpg",
+    url: "https://s.shopee.co.th/9fJBOrvOOE"
+  },
+  {
+    title: "ครีมกันแดดเวนิต้า VENITA + VENITA ANTI-ACNE PORE TIGHTENING ESSENCE TONER 150 ML",
+    price: "฿330.00",
+    image: "a/2.jpg",
+    url: "https://s.shopee.co.th/4fuVTosFwp"
+  },
+  {
+    title: "โปรโมชั่นสุดคุ้ม (ซื้อ 2 แถม 2 ) FEALI ครีมกันแดด UV Serum Sunscreen SPF50+ PA+++",
+    price: "฿250.00",
+    image: "a/3.png",
+    url: "https://s.shopee.co.th/6VM9feDQUW"
+  },
+  {
+    title: "ร้านบริษัท] KOTA KERATIN TREATMENT ทรีทเม้นท์เคารา ติน บำรุงเข้มข้น ผมนุ่มลื่น กลิ่นหอม ลดการหลุดร่วง",
+    price: "฿139.00",
+    image: "a/4.png",
+    url: "https://s.shopee.co.th/1BKdKjOiCS"
+  },
+  {
+    title: "Hydrating Mineral Sunscreen Face SPF50 75ml กันแดดมิเนอรัลสำหรับผิวหน้า เนื้อบางเบา",
+    price: "฿189.00",
+    image: "a/5.png",
+    url: "https://s.shopee.co.th/7AbqTtUC1u"
+  },
+  {
+    title: "CLEAR NOSE UV Sun Serum SPF50+ PA++++ Sun Booster 80ml.",
+    price: "฿350.00",
+    image: "a/6.png",
+    url: "https://s.shopee.co.th/111D9RyfWv"
+  },
+  {
+    title: "ครีมอาบน้ำผิวขาว ครีมอาบน้ำนมแพะ สบู่ตัวขาว 800ml ทำความสะอาดผิว กันเหงื่อ ผิวกระจ่างใส บำรุงให้ผิวขาวก...",
+    price: "฿299.00 ฿1,049.00",
+    image: "a/7.png",
+    url: "https://s.shopee.co.th/1Le3YDxtji"
+  },
+  {
+    title: "POPASKIN 577 + 377 Brightening Serum เซรั่มผิวขาว ลดรอยสิว จุดด่างดำ ฝ้า กระ",
+    price: "฿499.00",
+    image: "a/8.png",
+    url: "https://s.shopee.co.th/20tkLs2O4s"
+  },
+  {
+    title: "กันแดดโทนอัพผิวลูกคุณ Charmiss Paradise Island UV Body Serum Tone Up SPF50+ PA++++ 10...",
+    price: "฿249.00",
+    image: "a/9.png",
+    url: "https://s.shopee.co.th/8AUNhVbdV0"
+  },
+  {
+    title: "สบู่ลดกลิ่นคนแก่ กลิ่นตัวแรง กลิ่นฮอร์โมน กลิ่นตัวผู้ใหญ่ กลิ่นหลังใบหู สบู่ลูกพลับญี่ปุ่น pH5 Balansy 30+",
+    price: "฿439.00",
+    image: "a/10.png",
+    url: "https://s.shopee.co.th/6AjJMh4twI"
+  },
+  {
+    title: "แชมพูและครีมนวดผม Extra care Shampoo and Conditioner",
+    price: "฿460.00",
+    image: "a/11.png",
+    url: "https://s.shopee.co.th/4qDvxKIJF4"
+  },
+  {
+    title: "Merrezca moisture Lip Oil ลิปออยล์ บำรุงริมฝีปากให้ ชุ่มชื้น เงางาม ฉ่ำโกลว์",
+    price: "฿249.00",
+    image: "a/12.png",
+    url: "https://s.shopee.co.th/9pcbuj2LG3"
+  },
+  {
+    title: "2 ขวด แถม 2 ขวด ขนาดทดลอง ครีมอาบน้ำลดสิวแผ่น หลังและลำตัว ลดสิวอุดตัน DEOdore'สีเขียว",
+    price: "฿598.00",
+    image: "a/13.png",
+    url: "https://s.shopee.co.th/8fQeWnsfjM"
+  },
+  {
+    title: "ครีมอาบน้ำลดสิวแผ่นหลังและลำตัว สูตรลดรอย สิว DEOdore' shower cream Brightening",
+    price: "฿777.00",
+    image: "a/14.png",
+    url: "https://s.shopee.co.th/7fY7L7KgYl"
+  },
+  {
+    title: "APEX-SX WHITE UP CREAM ครีมทารักแร้ขาว โดยแพทย์ ผิวหนังเอเพ็กซ์ ทาขาหนีบดำ แก้มก้นดำ 50 ml.",
+    price: "฿990.00",
+    image: "a/15.png",
+    url: "https://s.shopee.co.th/8V7EL311MY"
+  },
+  {
+    title: "บอดี้สเปรย์ ลดกลิ่นตัวแรง กำจัดทุกกลิ่นกาย ทุกเพศ/ วัย กลิ่นคนแก่ จากลูกพลับญี่ปุ่น pH4 -บาลานซี่ 30+Anti ...",
+    price: "฿469.00",
+    image: "a/16.png",
+    url: "https://s.shopee.co.th/1BKdbjpurN"
+  },
+  {
+    title: "APEX-SX FEMM SERUM เซรั่มบำรุงให้ผิวน้องสาว สวย สุขภาพดี 50 ml.",
+    price: "฿1,490.00",
+    image: "a/17.png",
+    url: "https://s.shopee.co.th/4AyFBM6Fc5"
+  },
+  {
+    title: "Eucerin SPOTLESS BRIGHTENING THIAMIDOL BOOSTER SERUM 30 ML",
+    price: "฿2,250.00",
+    image: "a/18.png",
+    url: "https://s.shopee.co.th/9AMvAQOVFW"
+  },
+  {
+    title: "Banala Lite อุปกรณ์ช่วยนอนหลับ หลับง่าย หลับลึก หลับ สนิท ตื่นสดชื่นด้วยนวัตกรรมคลื่นเสียงอัจฉริยะ",
+    price: "฿1,790.00",
+    image: "a/19.png",
+    url: "https://s.shopee.co.th/4Vb5bxPgxE"
+  },
+  {
+    title: "ANCHI Electric bike จักรยานไฟฟ้า 580W วิ่งได้ เวลาใช้ งานแบตเตอรี่นาน เหมาะสำหรับผู้ใหญ่ นักเรียน ปลอดภัย",
+    price: "฿9,298.00 ฿10,458.00",
+    image: "a/20.png",
+    url: "https://s.shopee.co.th/7VEhBd6enC"
+  },
+  {
+    title: "สินค้าขายดี]นีเวีย ลูมินัส630 สกิน โกลว์ เซรั่ม เซรั่มผิวใส เซรั่มทาหน้า30 มล.2 ชิ้นNIVEA",
+    price: "฿892.00",
+    image: "a/21.png",
+    url: "https://s.shopee.co.th/6pz0ObEJcc"
+  },
+  {
+    title: "เซรั่มบำรุงผม",
+    price: "฿259",
+    image: "3.jpg",
+    url: "https://..."
+  },
+  {
+    title: "แชมพูสมุนไพร",
+    price: "฿199",
+    image: "2.jpg",
+    url: "https://..."
+  },
+  {
+    title: "เซรั่มบำรุงผม",
+    price: "฿259",
+    image: "3.jpg",
+    url: "https://..."
+  },
+  {
+    title: "เซรั่มบำรุงผม",
+    price: "฿330.00",
+    image: "3.jpg",
+    url: "https://..."
+  },
+  {
+    title: "แชมพูสมุนไพร",
+    price: "฿199",
+    image: "2.jpg",
+    url: "https://..."
+  },
+  {
+    title: "เซรั่มบำรุงผม",
+    price: "฿259",
+    image: "3.jpg",
+    url: "https://..."
+  },
+  {
+    title: "แชมพูสมุนไพร",
+    price: "฿199",
+    image: "2.jpg",
+    url: "https://..."
+  },
+  {
+    title: "เซรั่มบำรุงผม",
+    price: "฿259",
+    image: "3.jpg",
+    url: "https://..."
+  },
+  {
+    title: "แชมพูสมุนไพร",
+    price: "฿199",
+    image: "2.jpg",
+    url: "https://..."
+  },
+  {
+    title: "เซรั่มบำรุงผม",
+    price: "฿259",
+    image: "3.jpg",
+    url: "https://..."
+  },
+  {
+    title: "เซรั่มบำรุงผม",
+    price: "฿330.00",
+    image: "3.jpg",
+    url: "https://..."
+  },
+  {
+    title: "แชมพูสมุนไพร",
+    price: "฿199",
+    image: "2.jpg",
+    url: "https://..."
+  },
+  {
+    title: "เซรั่มบำรุงผม",
+    price: "฿259",
+    image: "3.jpg",
+    url: "https://..."
+  },
+  {
+    title: "แชมพูสมุนไพร",
+    price: "฿199",
+    image: "2.jpg",
+    url: "https://..."
+  },
+  {
+    title: "เซรั่มบำรุงผม",
+    price: "฿259",
+    image: "3.jpg",
+    url: "https://..."
+  },
+  {
+    title: "แชมพูสมุนไพร",
+    price: "฿199",
+    image: "2.jpg",
+    url: "https://..."
+  },
+  {
+    title: "เซรั่มบำรุงผม",
+    price: "฿259",
+    image: "3.jpg",
+    url: "https://..."
+  },
+  {
+    title: "เซรั่มบำรุงผม",
+    price: "฿330.00",
+    image: "3.jpg",
+    url: "https://..."
+  },
+  {
+    title: "แชมพูสมุนไพร",
+    price: "฿199",
+    image: "2.jpg",
+    url: "https://..."
+  },
+  {
+    title: "เซรั่มบำรุงผม",
+    price: "฿259",
+    image: "3.jpg",
+    url: "https://..."
+  },
+  {
+    title: "แชมพูสมุนไพร",
+    price: "฿199",
+    image: "2.jpg",
+    url: "https://..."
+  },
+  {
+    title: "เซรั่มบำรุงผม",
+    price: "฿259",
+    image: "3.jpg",
+    url: "https://..."
+  },
+  {
+    title: "แชมพูสมุนไพร",
+    price: "฿199",
+    image: "2.jpg",
+    url: "https://..."
+  },
+  {
+    title: "เซรั่มบำรุงผม",
+    price: "฿259",
+    image: "3.jpg",
+    url: "https://..."
+  },
+  {
+    title: "เซรั่มบำรุงผม",
+    price: "฿330.00",
+    image: "3.jpg",
+    url: "https://..."
+  },
+  {
+    title: "แชมพูสมุนไพร",
+    price: "฿199",
+    image: "2.jpg",
+    url: "https://..."
+  },
+  {
+    title: "เซรั่มบำรุงผม",
+    price: "฿259",
+    image: "3.jpg",
+    url: "https://..."
+  },
+  {
+    title: "แชมพูสมุนไพร",
+    price: "฿199",
+    image: "2.jpg",
+    url: "https://..."
+  },
+  {
+    title: "เซรั่มบำรุงผม",
+    price: "฿259",
+    image: "3.jpg",
+    url: "https://..."
+  },
+  {
+    title: "แชมพูสมุนไพร",
+    price: "฿199",
+    image: "2.jpg",
+    url: "https://..."
+  },
+  {
+    title: "เซรั่มบำรุงผม",
+    price: "฿259",
+    image: "3.jpg",
+    url: "https://..."
+  },
+  {
+    title: "เซรั่มบำรุงผม",
+    price: "฿330.00",
+    image: "3.jpg",
+    url: "https://..."
+  },
+  {
+    title: "แชมพูสมุนไพร",
+    price: "฿199",
+    image: "2.jpg",
+    url: "https://..."
+  },
+  {
+    title: "เซรั่มบำรุงผม",
+    price: "฿259",
+    image: "3.jpg",
+    url: "https://..."
+  },
+  {
+    title: "แชมพูสมุนไพร",
+    price: "฿199",
+    image: "2.jpg",
+    url: "https://..."
+  },
+  {
+    title: "เซรั่มบำรุงผม",
+    price: "฿259",
+    image: "3.jpg",
+    url: "https://..."
+  },
+  {
+    title: "แชมพูสมุนไพร",
+    price: "฿199",
+    image: "2.jpg",
+    url: "https://..."
+  },
+  {
+    title: "เซรั่มบำรุงผม",
+    price: "฿259",
+    image: "3.jpg",
+    url: "https://..."
+  },
+  {
+    title: "เซรั่มบำรุงผม",
+    price: "฿330.00",
+    image: "3.jpg",
+    url: "https://..."
+  },
+  {
+    title: "แชมพูสมุนไพร",
+    price: "฿199",
+    image: "2.jpg",
+    url: "https://..."
+  },
+  {
+    title: "เซรั่มบำรุงผม",
+    price: "฿259",
+    image: "3.jpg",
+    url: "https://..."
+  },
+  {
+    title: "แชมพูสมุนไพร",
+    price: "฿199",
+    image: "2.jpg",
+    url: "https://..."
+  },
+  {
+    title: "เซรั่มบำรุงผม",
+    price: "฿259",
+    image: "3.jpg",
+    url: "https://..."
+  },
+  {
+    title: "แชมพูสมุนไพร",
+    price: "฿199",
+    image: "2.jpg",
+    url: "https://..."
+  },
+  {
+    title: "เซรั่มบำรุงผม",
+    price: "฿259",
+    image: "3.jpg",
+    url: "https://..."
+  },
+  {
+    title: "เซรั่มบำรุงผม",
+    price: "฿330.00",
+    image: "3.jpg",
+    url: "https://..."
+  },
+  {
+    title: "แชมพูสมุนไพร",
+    price: "฿199",
+    image: "2.jpg",
+    url: "https://..."
+  },
+  {
+    title: "เซรั่มบำรุงผม",
+    price: "฿259",
+    image: "3.jpg",
+    url: "https://..."
+  },
+  {
+    title: "แชมพูสมุนไพร",
+    price: "฿199",
+    image: "2.jpg",
+    url: "https://..."
+  },
+  {
+    title: "เซรั่มบำรุงผม",
+    price: "฿259",
+    image: "3.jpg",
+    url: "https://..."
+  },
+  {
+    title: "แชมพูสมุนไพร",
+    price: "฿199",
+    image: "2.jpg",
+    url: "https://..."
+  },
+  {
+    title: "เซรั่มบำรุงผม",
+    price: "฿259",
+    image: "3.jpg",
+    url: "https://..."
+  },
+  {
+    title: "เซรั่มบำรุงผม",
+    price: "฿330.00",
+    image: "3.jpg",
+    url: "https://..."
+  },
+  {
+    title: "แชมพูสมุนไพร",
+    price: "฿199",
+    image: "2.jpg",
+    url: "https://..."
+  },
+  {
+    title: "เซรั่มบำรุงผม",
+    price: "฿259",
+    image: "3.jpg",
+    url: "https://..."
+  },
+  {
+    title: "แชมพูสมุนไพร",
+    price: "฿199",
+    image: "2.jpg",
+    url: "https://..."
+  },
+  {
+    title: "เซรั่มบำรุงผม",
+    price: "฿259",
+    image: "3.jpg",
+    url: "https://..."
+  },
+  {
+    title: "แชมพูสมุนไพร",
+    price: "฿199",
+    image: "2.jpg",
+    url: "https://..."
+  },
+  {
+    title: "เซรั่มบำรุงผม",
+    price: "฿259",
+    image: "3.jpg",
+    url: "https://..."
+  },
+  {
+    title: "เซรั่มบำรุงผม",
+    price: "฿330.00",
+    image: "3.jpg",
+    url: "https://..."
+  },
+  {
+    title: "แชมพูสมุนไพร",
+    price: "฿199",
+    image: "2.jpg",
+    url: "https://..."
+  },
+  {
+    title: "เซรั่มบำรุงผม",
+    price: "฿259",
+    image: "3.jpg",
+    url: "https://..."
+  },
+  {
+    title: "แชมพูสมุนไพร",
+    price: "฿199",
+    image: "2.jpg",
+    url: "https://..."
+  },
+  {
+    title: "เซรั่มบำรุงผม",
+    price: "฿259",
+    image: "3.jpg",
+    url: "https://..."
+  },
+  {
+    title: "แชมพูสมุนไพร",
+    price: "฿199",
+    image: "2.jpg",
+    url: "https://..."
+  },
+  {
+    title: "เซรั่มบำรุงผม",
+    price: "฿259",
+    image: "3.jpg",
+    url: "https://..."
+  },
+];
+
+let html = "";
+
+products.forEach(product => {
+  html += `
+    <div class="card">
+      <img src="${product.image}">
+      <div class="info">
+        <div class="title">${product.title}</div>
+        <div class="price">${product.price}</div>
+        <a class="button" href="${product.url}" target="_blank">
+          ดูสินค้า
+        </a>
+      </div>
+    </div>
+  `;
+});
+
+document.getElementById("ads").innerHTML = html;
